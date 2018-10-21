9ae3aff79f4e3cec899c10b86143906b79cd0195
Shawn Kim <shawnhan.kim@gmail.com>
2018-10-20 20:27:20 -0700

    Update message

--- sample/aws/sample_aws_create_instance.go
+++ sample/aws/sample_aws_create_instance.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // CreateAWSInstance creates an instance on AWS
 func CreateAWSInstance() error {
-	fmt.Printf("Create sample instance on AWS\n")
+	fmt.Printf("Create a sample instance on AWS\n")
 	return nil
 }
