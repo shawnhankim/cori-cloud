4bdf044c5afa1f568096da3758ea2449e46eaecc
Shawn Kim <shawnhan.kim@gmail.com>
2018-10-21 21:36:57 -0700

    Create instance, session, key pair on AWS

--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"fmt"
 
-	util "github.com/shawnhankim/cori-cloud/pkg/util"
 	sampleAWS "github.com/shawnhankim/cori-cloud/sample/aws"
 )
 
 func main() {
+
+	// Display title of cori-cloud
 	fmt.Printf("This is the cori-cloud.\n")
 
-	err := sampleAWS.CreateAWSInstance()
-	if err != nil {
-		util.CoriPrintf("Unable to create AWS instance! Error : %v", err)
-	} else {
-		util.CoriPrintf("Successfully created AWS instance! \n")
-	}
+	// Sample Code : create an instance on AWS
+	sampleAWS.CreateAWSInstance()
 }
--- sample/aws/sample_aws_create_instance.go
+++ sample/aws/sample_aws_create_instance.go
@@ -1,51 +1,45 @@
+// Copyright 2018 The Cori Cloud Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package sample provides code examples how to provision/deprovision resources
+// on the multiple cloud providers to help software engineers.
 package sample
 
 import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-
+	coriAWS "github.com/shawnhankim/cori-cloud/pkg/aws"
 	util "github.com/shawnhankim/cori-cloud/pkg/util"
 )
 
 // CreateAWSInstance creates an instance on AWS
 func CreateAWSInstance() error {
 
-	util.CoriPrintln("Create a sample instance on AWS")
-	sess, err := CreateNewSession()
+	// Display starting message
+	util.CoriPrintln("Start creating a sample instance on AWS.")
+
+	// Declare sample variables
+	sampleRegion := "us-west-2"
+	sampleProfile := "my-account"
+	sampleKeyName := "cori-key"
+
+	// Create an instance on AWS
+	err := coriAWS.CreateInstance(sampleRegion, sampleProfile, sampleKeyName)
 	if err != nil {
+		util.CoriPrintln("Unable to create a sample instance on AWS. ")
 		return err
 	}
-	err = CreateAWSKeyPair(sess)
+
+	// Display completion message
+	util.CoriPrintln("Successfully created a sample instance on AWS.")
 	return err
-}
-
-// CreateAWSKeyPair creates Key Pair on AWS
-func CreateAWSKeyPair(sess *session.Session) error {
-
-	svc := ec2.New(session.New())
-	input := &ec2.CreateKeyPairInput{
-		KeyName: aws.String("cori-key-pair"),
-	}
-
-	result, err := svc.CreateKeyPair(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return err
-	}
-
-	fmt.Println(result)
-	return nil
 }
